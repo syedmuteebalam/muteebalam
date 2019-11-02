82dd549d2af33fd9717d18a30f58f391b2786b33
Muteeb Alam <syedmuteebalam@gmail.com>
2019-11-02 22:54:42 +0500

    python introductory classes

---
+++ content/november-2.py
@@ -0,0 +1,59 @@
+print('hello world')
+print('hiiii')
+print('your mama')
+
+def add(a,b):
+    return a+b
+
+print(add('foo', 'bar'))
+
+
+def c2f(c):
+    return 9.0/5.0 * c + 32
+
+#  this is a comment
+print(c2f(12))
+
+
+def f2c(f):
+    return 5.0/9.0 * (f - 32) 
+
+
+print(f2c(25))
+
+
+
+
+
+def c2k(c):
+    return c + 273.15 
+
+
+print(c2k(25))
+
+
+
+
+def m2f(m):
+    return m * 3.281
+
+
+print(m2f(1)) 
+
+
+
+def f2m(f):
+    return f / 3.281
+
+
+print(f2m(1))
+
+
+
+
+    
+
+
+
+
+
